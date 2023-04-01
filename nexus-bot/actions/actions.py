95eff237411513c059d265f5d8f38f6eb63aeb89
Saivijayakumar <81847004+Saivijayakumar@users.noreply.github.com>
2023-04-01 20:36:20 +0530

    nexus api connected in action

--- nexus-bot/actions/actions.py
+++ nexus-bot/actions/actions.py
@@ -8,22 +8,31 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 # from typing import Any, Text, Dict, List
-#
+
 # from rasa_sdk import Action, Tracker
 # from rasa_sdk.executor import CollectingDispatcher
-#
-#
+# import requests
+
 # class ActionHelloWorld(Action):
-#
+
 #     def name(self) -> Text:
-#         return "action_hello_world"
-#
+#         return "action_leave_info"
+
 #     def run(self, dispatcher: CollectingDispatcher,
 #             tracker: Tracker,
 #             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
+        
+
+#         api_address='https://run.mocky.io/v3/a9a51f3e-cb03-4c16-ac0a-afb46f6a50e3'
+
+#         json_data = requests.get(api_address).json() 
+#         temp1=""
+
+#         for person in json_data:
+#             temp = (f"{json_data[person]['Name']} has {json_data[person]['Leaves']}.")
+#             temp1 = temp1+temp 
+#         print(temp1)
+#         dispatcher.utter_message(text=temp1)
 #         return []
 
 
@@ -44,14 +53,16 @@ class ActionHelloWorld(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
 
-        api_address='https://run.mocky.io/v3/a9a51f3e-cb03-4c16-ac0a-afb46f6a50e3'
+        url = "https://nexusuat.tvsnext.io:1180/api/Leaves/GetAvailableLeaveDetailsByEmployee?employeeID="
 
-        json_data = requests.get(api_address).json() 
-        temp1=""
 
-        for person in json_data:
-            temp = (f"{json_data[person]['Name']} has {json_data[person]['Leaves']}.")
-            temp1 = temp1+temp 
-        print(temp1)
-        dispatcher.utter_message(text=temp1)
+        headers = {
+            'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IkVGRjc4NDM0MUMzQTA1REFENEVBODYyNERBRjgzNTEzIiwidHlwIjoiYXQrand0In0.eyJuYmYiOjE2ODAyNjUxNzEsImV4cCI6MTY4NDE1MzE3MSwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwIiwiYXVkIjoiTmV4dXNBUEkiLCJjbGllbnRfaWQiOiJuZXh1c191aSIsImp0aSI6IjcwRTZBNDFCMzhFNTIyRjgwNkU3QzlGRTlBMkQxM0YxIiwiaWF0IjoxNjgwMjY1MTcxLCJzY29wZSI6WyJOZXh1c0FQSSJdfQ.pqOG4pU7QqWDZWw2Dz874gMyMw-O0xyrnfSf2L0yjCqv4vJYTb2N9URcrzw75wv6RVW7zsvRabRRiKz8WhKxgHCDWORZZyY_F5pNfRy9kPlcskPBqo3HCezUrWbt1EcgeCRKwT6D1F6tWudKXj9pd8iBj6VuhZVv8XeFiKhF0pn3OYpHofV_IsIRJuYzK895Ckn0w3RHLRR4dFoiHbz_kP1q--FNI7hvexDduzl2BicTu40m5rliZ1kfPyy4_lm9lyHl8b1yn4BMPIccLwjEUrAMzHlTonWUXeV4wG0i3mjWrr1eONfPkyk1ziNtqqIHRBc-r1ZOafJncU-BjSbwtw'
+        }
+
+        resp = requests.get(url+"1", headers=headers).json()
+
+        for data in resp['data']:
+            print(f"you have {data['availableLeaves']} Avilable Leaves from {data['leaveType']}")
+            dispatcher.utter_message(text=f"you have {data['availableLeaves']} Avilable Leaves from {data['leaveType']}")
         return []
